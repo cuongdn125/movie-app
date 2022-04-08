8dba5e643a9d01f1ecaf15925dba6aa104b17cbe
cuongdn125 <ngoccuong061@gmail.com>
2022-04-08 15:53:41 +0700

    upload

--- src/api/tmdbApi.js
+++ src/api/tmdbApi.js
@@ -38,14 +38,6 @@ const tmdbApi = {
     const url = category[cate] + '/' + id;
     return axiosClient.get(url, params);
   },
-  credits: (cate, id) => {
-    const url = category[cate] + '/' + id + '/credits';
-    return axiosClient.get(url, { params: {} });
-  },
-  similar: (cate, id) => {
-    const url = category[cate] + '/' + id + '/similar';
-    return axiosClient.get(url, { params: {} });
-  },
 };
 
 export default tmdbApi;
